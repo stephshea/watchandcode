3b12ae207181719820eea106e3e77ceb2a6634d6
Stephanie Shea <stephcshea@gmail.com>
2017-11-23 12:49:41 +0000

    version 5 complete

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var todoList = {
     todos: [],
     displayTodos: function() {
-        console.log('My todos: ', this.todos);
+        if (this.todos.length === 0) {
+            console.log('There are no todos');
+        }
+        
+        else {
+             console.log('My todos: ');
+             
+        for (var i = 0; i < this.todos.length; i++) {
+            if (this.todos[i].completed === true) {
+             console.log('(x)', this.todos[i].todoText);
+        }
+        else {
+             console.log('( )', this.todos[i].todoText);
+        }
+        }
+        }
     },
     addTodo: function(todoText) {
         this.todos.push({
