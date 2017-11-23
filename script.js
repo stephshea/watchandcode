4c4649bbdcfbba7daae83c4e99aa32fec4694d5c
Stephanie Shea <stephcshea@gmail.com>
2017-11-23 20:47:31 +0000

    version 6

--- script.js
+++ script.js
@@ -3,19 +3,15 @@ var todoList = {
     displayTodos: function() {
         if (this.todos.length === 0) {
             console.log('There are no todos');
-        }
-        
-        else {
+        } else {
              console.log('My todos: ');
-             
         for (var i = 0; i < this.todos.length; i++) {
             if (this.todos[i].completed === true) {
              console.log('(x)', this.todos[i].todoText);
-        }
-        else {
+        } else {
              console.log('( )', this.todos[i].todoText);
         }
-        }
+        } 
         }
     },
     addTodo: function(todoText) {
@@ -34,10 +30,33 @@ var todoList = {
     this.todos.splice(position, 1);
     this.displayTodos();
 },
-    toggleCompleted:  function(position) {
+    toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
-    }
-};
+    },
 
+    toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    for (var i = 0; i < totalTodos; i++) {
+        if (this.todos[i].completed === true) {
+            completedTodos++;
+        }
+    }
+    
+    if (completedTodos === totalTodos) {
+        for (var i = 0; i < totalTodos; i++) {
+            this.todos[i].completed = false;
+        }
+    } else {
+        for (var i = 0; i < totalTodos; i++) {
+            this.todos[i].completed =true;
+        }
+    }
+    this.displayTodos();
+    
+    
+    }
+
+};
